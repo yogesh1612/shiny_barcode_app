1f63f8b56a8baf7780024bb3d6b1428ebe98d66f
yogesh1612 <yogesh1612@users.noreply.github.com>
2021-04-08 18:16:52 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,1 +1,4 @@
 if (!require('baRcodeR')) {install.packages("baRcodeR")}; library('baRcodeR') 
+if (!require('shiny')) {install.packages("shiny")}; library('shiny') 
+if (!require('qrcode')) {install.packages("qrcode")}; library('qrcode') 
+
